74dc45d18db745c019afa181f134dafd1f5bac6d
ch <boych.0002@gmail.com>
2026-03-18 21:28:46 +0700

    register_round1

---
+++ app/layouts/log_regis.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="h-screen w-screen flex">
+        <!-- left -->
+        <div class="bg-gradient-to-t from-black to-yellow-500 w-1/2 h-full">
+            
+        </div>
+        <!-- right -->
+        <div class="bg-white w-1/2 h-full flex pt-10 px-10">
+            <slot />
+        </div>
+    </div>
+</template>
+
+
+<script setup lang="ts">
+
+</script>
---
+++ app/pages/register.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        a
+    </div>
+</template>
+
+
+<script setup lang="ts">
+definePageMeta({
+  layout: 'log-regis'
+})
+</script>
