1561fec65e9ec66866607edd7f54b568b8492c63
ch <boych.0002@gmail.com>
2026-03-28 22:31:14 +0700

    database_round_1

--- app/pages/register.vue
+++ app/pages/register.vue
@@ -1,66 +1,67 @@
 <template>
   <div>
     <p class="font-sarabun text-2xl font-bold pb-1">สร้างบัญชีใหม่</p>
+    <form @submit.prevent="handle_register">
+      <div class="bg-white p-4 border border-gray-300 rounded-2xl mt-4">
+        <p class="font-sarabun text-small font-bold">ข้อมูลบัญชี</p>
+        <div class="flex flex-col sm:flex-row gap-4 mt-2">
+          <!-- email -->
+          <div class="flex flex-col w-full sm:w-1/2">
+            <label for="email" class="font-bold text-gray-500 font-sarabun text-[13px] mb-1">อีเมล</label>
+            <input type="text" id="email" v-model="form.email" required
+              class="w-full h-10 border px-3 py-2 border-gray-300 rounded-xl font-sarabun text-sm focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500">
+          </div>
 
-    <div class="bg-white p-4 border border-gray-300 rounded-2xl mt-4">
-      <p class="font-sarabun text-small font-bold">ข้อมูลบัญชี</p>
+          <!-- username -->
+          <div class="flex flex-col w-full sm:w-1/2">
+            <label for="username" class="font-bold text-gray-500 font-sarabun text-[13px] mb-1">ชื่อผู้ใช้</label>
+            <input type="text" id="username" v-model="form.username" required
+              class="w-full h-10 border px-3 py-2 border-gray-300 rounded-xl font-sarabun text-sm focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500">
+          </div>
+        </div>
 
-      <div class="flex flex-col sm:flex-row gap-4 mt-2">
-        <!-- email -->
-        <div class="flex flex-col w-full sm:w-1/2">
-          <label for="email" class="font-bold text-gray-500 font-sarabun text-[13px] mb-1">อีเมล</label>
-          <input type="email" id="email"
+        <!-- password -->
+        <div class="font-sarabun mt-3">
+          <label for="password" class="block text-[13px] font-bold text-gray-500 mb-1">รหัสผ่าน</label>
+          <!-- ช่องใส่รหัสผ่าน -->
+          <input for="text" id="password" v-model="form.password" required
             class="w-full h-10 border px-3 py-2 border-gray-300 rounded-xl font-sarabun text-sm focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500">
         </div>
 
-        <!-- username -->
-        <div class="flex flex-col w-full sm:w-1/2">
-          <label for="username" class="font-bold text-gray-500 font-sarabun text-[13px] mb-1">ชื่อผู้ใช้</label>
-          <input type="text" id="username"
+        <!-- password_submit -->
+        <div class="font-sarabun mt-3">
+          <label for="password" class="block text-[13px] font-bold text-gray-500 mb-1">ยืนยันรหัสผ่าน</label>
+          <!-- ช่องใส่รหัสผ่าน -->
+          <input for="text" id="password" v-model="form.password" required
             class="w-full h-10 border px-3 py-2 border-gray-300 rounded-xl font-sarabun text-sm focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500">
         </div>
-      </div>
 
-      <!-- password -->
-      <div class="font-sarabun mt-3">
-        <label for="password" class="block text-[13px] font-bold text-gray-500 mb-1">รหัสผ่าน</label>
-        <!-- ช่องใส่รหัสผ่าน -->
-        <input for="text" id="password"
-          class="w-full h-10 border px-3 py-2 border-gray-300 rounded-xl font-sarabun text-sm focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500">
-      </div>
+        <button type="submit"
+          class="bg-[#1A1A1A] text-white p-3 w-full rounded-3xl font-sarabun font-bold hover:bg-[#404040] cursor-pointer transition-colors duration-300 ease-in-out mt-4">
+          ลงทะเบียน
+        </button>
 
-      <div class="font-sarabun mt-3">
-        <label for="password" class="block text-[13px] font-bold text-gray-500 mb-1">ยืนยันรหัสผ่าน</label>
-        <!-- ช่องใส่รหัสผ่าน -->
-        <input for="text" id="password"
-          class="w-full h-10 border px-3 py-2 border-gray-300 rounded-xl font-sarabun text-sm focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500">
-      </div>
+        <div class="flex items-center mt-5">
+          <div class="flex-grow border-t border-gray-300"></div>
+          <span class="flex-shrink mx-4 text-gray-500 font-bold font-sarabun text-sm">หรือ สมัครด้วย</span>
+          <div class="flex-grow border-t border-gray-300"></div>
+        </div>
 
-      <button type="submit"
-        class="bg-[#1A1A1A] text-white p-3 w-full rounded-3xl font-sarabun font-bold hover:bg-[#404040] cursor-pointer transition-colors duration-300 ease-in-out mt-4">
-        ลงทะเบียน
-      </button>
+        <div class="mt-4 mb-4 w-full flex justify-center ">
+          <!-- gmail -->
+          <div
+            class="bg-white w-1/3  p-2 flex justify-center items-center rounded-3xl cursor-pointer hover:shadow-md hover:scale-105 transition-all">
+            <div class="w-12 h-[2rem]">
+              <img src="../assets/images/icon_google.png" alt="" class="w-full h-full object-cover">
+            </div>
 
-      <div class="flex items-center mt-5">
-        <div class="flex-grow border-t border-gray-300"></div>
-        <span class="flex-shrink mx-4 text-gray-500 font-bold font-sarabun text-sm">หรือ สมัครด้วย</span>
-        <div class="flex-grow border-t border-gray-300"></div>
-      </div>
-
-      <div class="mt-4 mb-4 w-full flex justify-center ">
-        <!-- gmail -->
-        <div
-          class="bg-white w-1/3  p-2 flex justify-center items-center rounded-3xl cursor-pointer hover:shadow-md hover:scale-105 transition-all">
-          <div class="w-12 h-[2rem]">
-            <img src="../assets/images/icon_google.png" alt="" class="w-full h-full object-cover">
-          </div>
-
-          <div class="font-sarabun">
-            <p>Sign up to Google</p>
+            <div class="font-sarabun">
+              <p>Sign up to Google</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </form>
   </div>
 </template>
 
@@ -69,4 +70,14 @@
 definePageMeta({
   layout: 'log-regis'
 })
+
+const form = ref({
+  email: '',
+  username: '',
+  password: ''
+})
+
+const handle_register = async () => {
+  console.log('ข้อมูลพร้อมส่งไป API แล้ว!', form.value);
+}
 </script>
--- server/utils/db.ts
+++ server/utils/db.ts
@@ -2,7 +2,7 @@ import pkg from 'pg'
 
 const {Pool} = pkg;
 
-const pool = new Pool({
+export const pool = new Pool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
