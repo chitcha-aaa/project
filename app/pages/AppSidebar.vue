f35b08c7ab01679b377e3a1e04f2c5503b0c3bfe
ch <boych.0002@gmail.com>
2026-04-07 18:31:52 +0700

    sidebar_round1

---
+++ app/pages/AppSidebar.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="h-screen flex ">
+        <aside class="w-56 h-full bg-gray-300 flex flex-col p-2">
+            <div class="bg-gray-500 font-sarabun">
+                <span class="font-bold text-3xl mb-1">BRICC</span>
+                <span class="ml-1 text-[12px]">(ครั้งที่12)</span>
+                <p class="text-sm">มหาวิทยาลัยราชภัฏบุรีรัมย์</p>
+
+                <div class="flex items-center">
+                    <div class="flex-grow border-t border-gray-500"></div>
+                </div>
+            </div>
+        </aside>
+    </div>
+</template>
+
+<script setup lang="ts">
+    
+</script>
