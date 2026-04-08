f5c033157e4c80a436fe4af24b3158afc972f0d4
ch <boych.0002@gmail.com>
2026-04-08 17:59:21 +0700

    add navbar round 2

--- app/pages/AppSidebar.vue
+++ app/pages/AppSidebar.vue
@@ -1,19 +1,44 @@
 <template>
-    <div class="h-screen flex ">
-        <aside class="w-56 h-full bg-gray-300 flex flex-col p-2">
-            <div class="bg-gray-500 font-sarabun">
-                <span class="font-bold text-3xl mb-1">BRICC</span>
-                <span class="ml-1 text-[12px]">(ครั้งที่12)</span>
-                <p class="text-sm">มหาวิทยาลัยราชภัฏบุรีรัมย์</p>
-
-                <div class="flex items-center">
-                    <div class="flex-grow border-t border-gray-500"></div>
-                </div>
+    <div class="h-screen flex">
+        <div class="flex flex-col w-56 h-full bg-gray-200">
+            <div class="size-14 grow-3 bg-gray-400">                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           >
+                
             </div>
-        </aside>
+            <div class="size-14 grow-3 bg-gray-400">
+
+
+            </div>
+            <div class="size-14 grow-3 bg-gray-400">
+                    s
+            </div>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
-    
+
 </script>
+
+
+<!-- <div class="h-screen flex ">
+        <aside class="w-56 h-full bg-gray-300 flex flex-col p-2">
+            <div class="font-sarabun">
+                <span class="font-bold text-3xl mb-1">BRICC</span>
+                <span class="ml-1 text-[12px]">(ครั้งที่12)</span>
+                <p class="text-sm">มหาวิทยาลัยราชภัฏบุรีรัมย์</p>
+            </div>
+
+            <div class="flex items-center my-3">
+                <div class="flex-grow border-t border-gray-500"></div>
+                <div class="flex-grow border-t border-gray-500"></div>
+            </div>
+
+          
+            <div class="flex flex-col">
+                <div class="bg-gray-200 w-full h-10">
+
+                </div>
+            </div>
+        </aside>
+    </div>
+-->
