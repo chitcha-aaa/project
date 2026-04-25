ac1fd09b0787287dda96c88c9deca04587152036
ch <boych.0002@gmail.com>
2026-04-25 22:20:49 +0700

    update navbar_user round 5

--- app/pages/author/SubmitArticle.vue
+++ app/pages/author/SubmitArticle.vue
@@ -1,132 +1,155 @@
 <template>
     <Navbar />
 
-    <!-- <div class="mt-2 w-full h-20  flex justify-center items-center">
-        <div class="px-4 font-sarabun w-full max-w-[1395px]">
-            <p class="font-semibold text-xl">ส่งบทความ</p>
-            <p class="text-sm">กรุณากรอกข้อมูลให้ครบ 4 ขั้นตอน</p>
-        </div>
-    </div>
-
-    <div class="bg-gray-200 h-15 w-full flex justify-center">
-        <div class="bg-gray-300 w-full h-full max-w-[1395px] px-4">
-            เป็นการบอกลำดับขัั้นตอน 1-4 
-        </div>
-    </div>
-
-    <div class="w-full h-30  mt-2 px-4 font-sarabun flex justify-center">
-        <div class="border border-gray-300 rounded-xl w-full">
-            g
-        </div>
-    </div> -->
-
-    <div class="min-h-screen bg-gray-50 py-6 px-4 md:py-10 font-sarabun text-slate-800">
-        <div class="max-w-5xl mx-auto flex flex-col gap-8">
-
-            <header class="flex items-center gap-4">
-                <button
-                    class="w-12 h-12 rounded-full bg-white border border-gray-200 flex items-center justify-center shadow-sm hover:bg-gray-50 transition">
-                    <svg class="w-6 h-6 text-gray-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M10 19l-7-7m0 0l7-7m-7 7h18"></path>
-                    </svg>
-                </button>
-                <div>
-                    <h1 class="text-2xl font-bold text-gray-900">ส่งบทความใหม่ (New Submission)</h1>
-                    <p class="text-gray-500 text-sm">กรุณากรอกข้อมูลให้ครบถ้วนทั้ง 4 ขั้นตอน</p>
-                </div>
-            </header>
-
-            <div class="flex items-center justify-between w-full max-w-4xl mx-auto px-4 mt-4">
-                <div class="flex flex-col items-center relative">
-                    <div
-                        class="w-12 h-12 rounded-full bg-indigo-600 text-white flex items-center justify-center font-bold text-lg z-10 shadow-md">
-                        1</div>
-                    <span
-                        class="absolute top-14 text-indigo-700 font-bold text-sm whitespace-nowrap">ข้อมูลทั่วไป</span>
-                </div>
-                <div class="flex-1 h-1 bg-gray-200 mx-2 md:mx-4"></div>
-                <div class="flex flex-col items-center relative">
-                    <div
-                        class="w-12 h-12 rounded-full bg-white border-2 border-gray-200 text-gray-400 flex items-center justify-center font-bold text-lg z-10">
-                        2</div>
-                    <span
-                        class="absolute top-14 text-gray-400 font-medium text-sm whitespace-nowrap hidden sm:block">ผู้แต่ง</span>
-                </div>
+    <div class="min-h-screen bg-gray-100 font-sarabun py-6 px-4">
+        <div class="max-w-5xl mx-auto flex flex-col gap-2">
+            <div class="">
+                <h1 class="font-semibold text-2xl">ส่งบทความใหม่</h1>
+                <p class="text-sm text-gray-500">กรุณากรอกให้ครบถ้วนทั้ง 4 ขั้นตอน</p>
             </div>
 
-            <div class="bg-white rounded-[2rem] border border-gray-100 shadow-sm p-6 md:p-10 mt-8">
-                <div class="flex items-center gap-3 mb-8">
-                    <h2 class="text-xl font-bold text-gray-800">ข้อมูลทั่วไป (General Info)</h2>
-                </div>
+            <form @submit.prevent="">
+                <div class="border border-gray-300 rounded-2xl py-4 px-5">
+                    <div>
+                        <h1 class="font-semibold text-l">ข้อมูลทั่วไป</h1>
+                    </div>
 
-                <div class="grid grid-cols-1 md:grid-cols-2 gap-x-10 gap-y-8">
+                    <div class=" grid grid-cols-1 gap-4 md:grid-cols-2">
+                        <div>
+                            <label for="" class="text-sm font-semibold text-gray-700">ระดับผู้ส่งผลงาน</label>
+                            <div class="flex gap-2 mt-1">
+                                <button
+                                    class="p-2 flex-1 rounded-xl text-center bg-gray-200 text-sm 
+                                                focus:bg-yellow-400 focus:text-gray-900 hover:cursor-pointer hover:bg-gray-300">ปริญญาตรี</button>
+                                <button
+                                    class="p-2 flex-1 rounded-xl text-center bg-gray-200 text-sm 
+                                                focus:bg-yellow-400 focus:text-gray-900 hover:cursor-pointer hover:bg-gray-300">บุคคลทั่วไป
+                                    / นักวิจัย</button>
+                            </div>
+                        </div>
 
-                    <div class="flex flex-col gap-2">
-                        <label class="font-bold text-gray-700">ระดับผู้ส่งผลงาน</label>
-                        <div class="flex gap-4">
-                            <button
-                                class="flex-1 py-3 border border-gray-200 rounded-xl hover:border-indigo-500 focus:border-indigo-600 focus:ring-1 focus:ring-indigo-600 transition">ปริญญาตรี</button>
-                            <button
-                                class="flex-1 py-3 border border-gray-200 rounded-xl hover:border-indigo-500 transition">บุคคลทั่วไป
-                                / นักวิจัย</button>
+                        <div>
+                            <label for="" class="text-sm font-semibold text-gray-700">รูปแบบการนำเสนอ</label>
+                            <div class="flex gap-2 mt-1">
+                                <button
+                                    class="p-2 flex-1 rounded-xl text-center bg-gray-200 text-sm 
+                                                focus:bg-yellow-400 focus:text-gray-900 hover:cursor-pointer hover:bg-gray-300">Online</button>
+                                <button
+                                    class="p-2 flex-1 rounded-xl text-center bg-gray-200 text-sm 
+                                                focus:bg-yellow-400 focus:text-gray-900 hover:cursor-pointer hover:bg-gray-300">Onsite</button>
+                            </div>
+                        </div>
+
+                        <!-- ประเภทบทความ -->
+                        <div>
+                            <label for="" class="text-sm font-semibold text-gray-700">ประเภทบทความ</label>
+                            <div ref="dropdownContainer" @click="toggleDropdown"
+                                class="relative bg-slate-300 w-full max-w-[300px] h-10 p-2 rounded-xl flex items-center mt-1 hover:cursor-pointer">
+                                <div class="text-sm"
+                                    :class="selectedOption ? 'text-gray-900 font-medium' : 'text-slate-600'">
+                                    <p>{{ selectedOption || 'โปรดเลือกประเภท' }}</p>
+                                </div>
+                                <div id="dropdown_icon" class="w-5 h-5 ml-auto flex justify-center items-center transition-transform duration-300">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="size-6"
+                                        :class="{ 'rotate-180': isOpen }">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                                    </svg>
+                                </div>
+
+                                <!-- กล่องเมนู -->
+                                <div v-show="isOpen"
+                                    class="absolute left-0 top-full mt-1 w-full bg-slate-300 rounded-xl shadow-lg z-10 overflow-hidden p-2">
+                                    <ul class="text-sm text-black">
+                                        <li v-for="(option, index) in options" :key="index"
+                                            @click.stop="selectOption(option)"
+                                            class="px-4 py-2 hover:bg-slate-100 cursor-pointer transition-colors rounded-xl ">
+                                            {{ option }}
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+
+
+                        </div>
+
+                        <!-- กลุ่มสาขาวิชา -->
+                        <div>
+                            <label for="" class="text-sm font-semibold text-gray-700">กลุ่มสาขาวิชา</label>
+                            <div ref="dropdownContainer" @click="toggleDropdown"
+                                class="relative bg-slate-300 w-full max-w-[300px] h-10 p-2 rounded-xl flex items-center mt-1 hover:cursor-pointer">
+                                <div class="text-sm"
+                                    :class="selectedOption ? 'text-gray-900 font-medium' : 'text-slate-600'">
+                                    <p>{{ selectedOption || 'โปรดเลือกกลุ่มสาขาวิชา' }}</p>
+                                </div>
+                                <div id="dropdown_icon" class="w-5 h-5 ml-auto flex justify-center items-center transition-transform duration-300">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="size-6"
+                                        :class="{ 'rotate-180': isOpen }">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                                    </svg>
+                                </div>
+
+                                <!-- กล่องเมนู -->
+                                <div v-show="isOpen"
+                                    class="absolute left-0 top-full mt-1 w-full bg-slate-300 rounded-xl shadow-lg z-10 overflow-hidden p-2">
+                                    <ul class="text-sm text-black">
+                                        <li v-for="(option, index) in options" :key="index"
+                                            @click.stop="selectOption(option)"
+                                            class="px-4 py-2 hover:bg-slate-100 cursor-pointer transition-colors rounded-xl ">
+                                            {{ option }}
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
                         </div>
                     </div>
-
-                    <div class="flex flex-col gap-2">
-                        <label class="font-bold text-gray-700">รูปแบบการนำเสนอ</label>
-                        <div class="flex gap-4">
-                            <button class="flex-1 py-3 border border-gray-200 rounded-xl transition">On-site</button>
-                            <button class="flex-1 py-3 border border-gray-200 rounded-xl transition">Online</button>
-                        </div>
-                    </div>
-
-                    <div class="flex flex-col gap-2">
-                        <label class="font-bold text-gray-700">ประเภทบทความ</label>
-                        <select
-                            class="w-full bg-gray-200/60 border border-gray-200 rounded-xl p-3 outline-none focus:border-indigo-500">
-                            <option>Oral Presentation</option>
-                        </select>
-                    </div>
-
-                    <div class="flex flex-col gap-2">
-                        <label class="font-bold text-gray-700">กลุ่มสาขาวิชา</label>
-                        <select
-                            class="w-full bg-gray-200/60 border border-gray-200 rounded-xl p-3 outline-none focus:border-indigo-500">
-                            <option>Computer Science & IT</option>
-                        </select>
-                    </div>
-
                 </div>
-
-                <div class="h-px bg-gray-100 my-8"></div>
-
-                <div class="grid grid-cols-1 md:grid-cols-2 gap-x-10 gap-y-8">
-                    <div class="flex flex-col gap-2">
-                        <label class="font-bold text-gray-700">ชื่อบทความ (ภาษาไทย)</label>
-                        <input type="text"
-                            class="w-full bg-transparent border border-gray-200 rounded-xl p-3 outline-none focus:border-indigo-500" />
-                    </div>
-                    <div class="flex flex-col gap-2">
-                        <label class="font-bold text-gray-700">ชื่อบทความ (ภาษาอังกฤษ)</label>
-                        <input type="text"
-                            class="w-full bg-transparent border border-gray-200 rounded-xl p-3 outline-none focus:border-indigo-500" />
-                    </div>
-                </div>
-
-            </div>
-
-            <div class="flex justify-between items-center px-2">
-                <button class="text-gray-500 font-bold hover:text-gray-800 transition">ยกเลิก</button>
-                <button
-                    class="bg-[#5C45FD] text-white px-8 py-3 rounded-xl font-bold shadow-lg shadow-indigo-200 hover:bg-indigo-700 transition flex items-center gap-2">
-                    ถัดไป
-                    <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
-                    </svg>
-                </button>
-            </div>
-
+            </form>
         </div>
     </div>
 </template>
+
+<script setup>
+import { ref, onMounted, onUnmounted } from 'vue'
+
+const isOpen = ref(false) //สถานะ เปิด/ปิดเมนู
+const selectedOption = ref(''); //ค่าที่ถูกเลือก
+const dropdownContainer = ref(null)
+
+const options = [
+    'งานวิจัย',
+    'บทความวิชาการ',
+    'กรณีศึกษา'
+]
+
+const toggleDropdown = () => {
+    isOpen.value = !isOpen.value
+}
+
+// 3. ฟังก์ชันเมื่อผู้ใช้คลิกเลือก
+const selectOption = (option) => {
+    selectedOption.value = option // อัปเดตค่าที่เลือก
+    isOpen.value = false // ปิดเมนู
+}
+
+// 4. ฟังก์ชันปิดเมนูเมื่อคลิกบริเวณอื่นของหน้าจอ
+const handleClickOutside = (event) => {
+    // ถ้าเมนูเปิดอยู่ และ จุดที่คลิก "ไม่ได้อยู่" ในกล่อง dropdownContainer
+    if (isOpen.value && dropdownContainer.value && !dropdownContainer.value.contains(event.target)) {
+        isOpen.value = false // สั่งปิด
+    }
+}
+
+// ติดตั้ง Event Listener เมื่อ Component ถูกโหลดเสร็จ
+onMounted(() => {
+    document.addEventListener('click', handleClickOutside)
+})
+
+// ถอด Event Listener ออกเมื่อ Component ถูกทำลาย (กันกินแรม)
+onUnmounted(() => {
+    document.removeEventListener('click', handleClickOutside)
+})
+</script>
